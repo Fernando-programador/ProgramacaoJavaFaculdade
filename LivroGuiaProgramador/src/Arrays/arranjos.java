99fe3765b365b7f3e224dc26f03708eceecab50b
Fernando Corrêa <suporte@ajeitandoseulado.com.br>
2022-05-17 00:34:40 -0300

    java
    atualização pasta java 17/05/2022

---
+++ LivroGuiaProgramador/src/Arrays/arranjos.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class arranjos {
+
+	public static void main(String[] args) {
+		int numero[] = new int [10];
+		double[] real = new double [10];
+		
+		
+		
+
+		
+	}
+
+}
